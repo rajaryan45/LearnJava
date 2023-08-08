c926f8269488182973bb014d41014806e52e57b4
Raj Aryan Behera <rajaryanbehera@gmail.com>
2023-08-08 14:34:47 +0530

    first

---
+++ src/com/learn/Methods.java
@@ -0,0 +1,19 @@
+package com.learn;
+
+public class Methods {
+	 
+	static int max(int x , int y) {
+		if(x>y)
+			return x;
+		else {
+			return y; 
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		int a = 10 , b = 15 ; 
+		System.out.println(max(a, b));
+	}
+
+}
