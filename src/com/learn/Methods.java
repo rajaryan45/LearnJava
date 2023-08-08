941c8cf5f72e6ab6e00435a5f9c37e57797b3d0d
Raj Aryan Behera <rajaryanbehera@gmail.com>
2023-08-08 14:45:16 +0530

    first

--- src/com/learn/Methods.java
+++ src/com/learn/Methods.java
@@ -9,11 +9,23 @@ public class Methods {
 			return y; 
 		}
 	}
+	public int maxNonStatic(int x , int y) { // example for non static
+		if(x>y)
+			return x;
+		else {
+			return y; 
+		}
+	}
 	
 	public static void main(String[] args) {
 		
 		int a = 10 , b = 15 ; 
-		System.out.println(max(a, b));
+		System.out.println("Max number : "+max(a, b));// Static function can only call static funtions 
+		/*
+		 * so to access non Static methods you have to create objects like below given exampple
+		 */
+		Methods objMethods = new Methods();
+		System.out.println("MAx number (non static way ): " +objMethods.maxNonStatic(a, b));
 	}
 
 }
